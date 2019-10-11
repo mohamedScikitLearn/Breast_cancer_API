21ab34b76883ffacd579052856685e2aba0434f9
Mohamed Berrimi <mohamedberrimi0@gmail.com>
2019-10-11 12:05:46 +0100

    edit API

--- API-file.py
+++ API-file.py
@@ -67,23 +67,7 @@ def f1(y_true, y_pred):
     recallx = recall(y_true, y_pred)
     return 2*((precisionx*recallx)/(precisionx+recallx+K.epsilon()))
 
-# SE block
-#def squeeze_excite_block(input, ratio=16):
-#    init = input
-#    channel_axis = 1 if K.image_data_format() == "channels_first" else -1
-#    filters = init._keras_shape[channel_axis]
-#    se_shape = (1, 1, filters)
-#
-#    se = GlobalAveragePooling2D()(init)
-#    se = Reshape(se_shape)(se)
-#    se = Dense(filters // ratio, activation='relu', kernel_initializer='he_normal', use_bias=False)(se)
-#    se = Dense(filters, activation='sigmoid', kernel_initializer='he_normal', use_bias=False)(se)
-#
-#    if K.image_data_format() == 'channels_first':
-#        se = Permute((3, 1, 2))(se)
-#
-#    x = multiply([init, se])
-#    return x
+
 
 def create_model():
    
